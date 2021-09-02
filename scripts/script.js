beecf727b76b2c9d0af910985fd2be7925246d0e
Benjamin JEANNEROT <benjamin.jeannerot@gmail.com>
2021-09-02 19:19:25 +0200

    page modif text en js au survol souris

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+// écouter un événement et produire une action 
+
+function  maFonction(e)
+{
+    console.log('click')
+}
+
+document.querySelector('article[data-order="3"] > label').addEventListener('click', maFonction);
+document.querySelector('article[data-order="3"] > label').addEventListener('click', e =>{console.log('clac');});
+
+
+
+// modifier en javascript l'apparence du texte selon événement de souris
+
+let lesArticles = document.querySelectorAll ("article") ;
+
+for (let articleEnCours of lesArticles)
+{
+    articleEnCours.addEventListener (
+        'mouseenter', e => {
+            e.currentTarget.style.fontWeight = 'bold';
+            e.currentTarget.fontStyle = 'italic';
+            e.currentTarget.style.color ='dodgerblue';
+        }
+    );
+    articleEnCours.addEventListener (
+        'mouseleave', e => {
+            e.currentTarget.style.fontWeight = 'normal';
+            e.currentTarget.style.fontStyle = 'normal';
+            e.currentTarget.style.color = 'black';
+        }
+    );
+}
